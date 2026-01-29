036514b3dff0c53269c0480219f43b0546f1783d
Sanor Smith <sanorsmith83@gmail.com>
2026-01-29 20:25:31 +0100

    Fix router URLs with proper base configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -70,7 +70,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
